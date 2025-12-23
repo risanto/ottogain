9839a834f287312508cdac997e08d45404fb6590
risan <potallegta@gmail.com>
2025-12-23 10:48:35 +0700

    feat: contact page

--- src/components/contactUs/form.tsx
+++ src/components/contactUs/form.tsx
@@ -1,29 +1,76 @@
 import { useTranslation } from "react-i18next";
 import "../../i18n";
 import { Button, Input, Textarea } from "@material-tailwind/react";
+import bgContactForm from "../../assets/partnership/bg-services-content.svg";
+import envelopeIcon from "../../assets/icons/envelope-open.svg";
+import mapMarkerIcon from "../../assets/icons/map-marker.svg";
+import phoneIcon from "../../assets/icons/phone.svg";
+import whatsappIcon from "../../assets/icons/whatsapp.svg";
+
+import { Select, Option } from "@material-tailwind/react";
+import { useState } from "react";
 
 export function Form() {
   const { t } = useTranslation();
 
+  function NeedSelect() {
+    const [value, setValue] = useState<string | undefined>("");
+
+    return (
+      <Select
+        value={value}
+        onChange={(val) => setValue(val)}
+        className="bg-white/10 lg:p-[1.8rem] !border-t-blue-gray-200 focus:!border-t-gray-900 rounded-xl text-white/70 lg:text-xl"
+        labelProps={{
+          className: "before:content-none after:content-none",
+        }}
+        menuProps={{
+          className: "text-xl",
+        }}
+      >
+        <Option value="" className="p-2">
+          <div className="px-4">{t("contactForm.purpose")}</div>
+        </Option>
+
+        <Option value="registrasi" className="p-2">
+          <div className="px-4">{t("contactForm.registration")}</div>
+        </Option>
+
+        <Option value="signal-provider" className="p-2">
+          <div className="px-4">Signal Provider</div>
+        </Option>
+
+        <Option value="deposit" className="p-2">
+          <div className="px-4">Deposit</div>
+        </Option>
+
+        <Option value="withdrawal" className="p-2">
+          <div className="px-4">Withdrawal</div>
+        </Option>
+
+        <Option value="legal" className="p-2">
+          <div className="px-4">Legal</div>
+        </Option>
+
+        <Option value="lainnya" className="p-2">
+          <div className="px-4">{t("contactForm.others")}</div>
+        </Option>
+      </Select>
+    );
+  }
+
   return (
     <>
-      <section className="px-6 py-12 bg-black grid gap-2.5 lg:gap-7 lg:grid-cols-2 lg:items-end lg:px-[6.25rem] lg:pt-[6.25rem] lg:pb-[4.3rem]">
-        <article
-          style={{
-            background:
-              "linear-gradient(142.52deg, #FFFFFF 38.78%, #D4D4D4 70.83%, #FFFFFF 97.47%)",
-          }}
-          className="p-[2rem] pb-[1.3rem] rounded-[20px] flex flex-col items-center relative lg:items-baseline lg:rounded-[40px] lg:p-[3.5rem] lg:pb-[4rem]"
-        >
-          <h2 className="text-[1.7rem] font-semibold text-left w-full lg:text-[3rem]">
-            {t("contactForm.h2")}
-          </h2>
-
-          <form className="grid gap-[0.8rem] w-full mt-[1.4rem] lg:mt-[2.4rem] lg:gap-[2.2rem]">
+      <section
+        className="gap-2.5 lg:gap-28 grid lg:grid-cols-2 bg-cover bg-center px-6 lg:px-[6.25rem] py-12 lg:pt-[6.25rem] lg:pb-20"
+        style={{ backgroundImage: `url(${bgContactForm.src})` }}
+      >
+        <article className="relative flex flex-col justify-center items-center lg:items-baseline bg-[#1B0A31] p-[2rem] lg:px-11 lg:pt-8 pb-[1.3rem] lg:pb-16 border border-[#BB7CE4]/30 rounded-[20px] lg:rounded-[40px]">
+          <form className="gap-[0.8rem] lg:gap-[2.2rem] grid mt-[1.4rem] lg:mt-[2.4rem] w-full">
             <Input
               size="lg"
               placeholder={t("contactForm.name")}
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 bg-white rounded-xl lg:text-2xl lg:p-[1.8rem]"
+              className="bg-white/10 lg:p-[1.8rem] !border-t-blue-gray-200 focus:!border-t-gray-900 rounded-xl text-white lg:text-xl"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
@@ -31,75 +78,72 @@ export function Form() {
             <Input
               size="lg"
               placeholder={t("contactForm.email")}
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 bg-white rounded-xl lg:text-2xl lg:p-[1.8rem]"
+              className="bg-white/10 lg:p-[1.8rem] !border-t-blue-gray-200 focus:!border-t-gray-900 rounded-xl text-white lg:text-xl"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
             />
             <Input
               size="lg"
-              placeholder={t("contactForm.company")}
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 bg-white rounded-xl lg:text-2xl lg:p-[1.8rem]"
+              placeholder={t("contactForm.whatsapp")}
+              className="bg-white/10 lg:p-[1.8rem] !border-t-blue-gray-200 focus:!border-t-gray-900 rounded-xl text-white lg:text-xl"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
             />
 
-            <Textarea
-              size="lg"
-              placeholder={t("contactForm.message")}
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 bg-white rounded-xl lg:text-2xl lg:p-[1.8rem]"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
+            <NeedSelect />
           </form>
 
-          <div className="flex flex-end justify-end w-full mt-[1.5rem] lg:mt-[2.8rem]">
-            <div className="p-[1px] rounded-full bg-[linear-gradient(to_right,_#905510_0%,_#F3CB5F_50%,_#A16B22_100%)] inline-block">
-              <Button
-                className="flex items-center rounded-full bg-gold-gradient text-white normal-case font-normal w-[10rem] justify-center text-[0.9rem] shadow-[0px_3px_5px_1px_rgba(0,0,0,0.3)] hover:bg-none hover:bg-black lg:text-[1.5rem] lg:w-[293px] lg:py-[1.3rem]"
-                variant="text"
-              >
-                {t("contactForm.btn")}
-              </Button>
+          <div className="flex justify-center items-center w-full">
+            <Button className="bg-gradient-to-r from-[#C47EFC] to-[#0F17C3] mt-16 px-16 py-4 font-semibold normal-case">
+              {t("contactForm.send")}
+            </Button>
+          </div>
+        </article>
+
+        <article className="relative gap-7 grid rounded-[20px] lg:rounded-[40px] text-white">
+          <h2 className="font-semibold text-5xl">{t("contactUsForm.h2")}</h2>
+
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9434.393062338417!2d106.78414750734926!3d-6.174427664205013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f74ca2c1fefb%3A0x66eb07e0fb3775c1!2sSOHO%20Podomoro%20City!5e0!3m2!1sen!2sid!4v1766391310189!5m2!1sen!2sid"
+            width="520"
+            height="259"
+            loading="lazy"
+            className="rounded-3xl"
+          ></iframe>
+
+          <div className="font-semibold text-3xl">HQ Office</div>
+
+          <div className="flex flex-col gap-5 w-[350px]">
+            <div className="flex gap-5">
+              <img
+                src={mapMarkerIcon.src}
+                alt="map marker icon"
+                className="w-4"
+              />
+              <span>
+                Soho Podomorocity lt.16 Central Park Area Jl. S Parman-Jakarta
+                barat Indonesia
+              </span>
+            </div>
+
+            <div className="flex gap-5">
+              <img src={whatsappIcon.src} alt="whatsapp icon" className="w-4" />
+              <span>+62811-3118-6888</span>
+            </div>
+
+            <div className="flex gap-5">
+              <img src={phoneIcon.src} alt="phone icon" className="w-4" />
+              <span>021-5035-1688</span>
+            </div>
+
+            <div className="flex gap-5">
+              <img src={envelopeIcon.src} alt="envelope icon" className="w-4" />
+              <span>support@ottogain.com</span>
             </div>
           </div>
         </article>
-
-        <article
-          style={{
-            background:
-              "linear-gradient(142.52deg, #FFFFFF 38.78%, #D4D4D4 70.83%, #FFFFFF 97.47%)",
-          }}
-          className="px-[1.6rem] py-[2.5rem] rounded-[20px] grid gap-3.5 relative font-semibold text-sm lg:text-2xl lg:max-h-[380px] lg:items-baseline lg:rounded-[40px] lg:px-[3.4rem] lg:py-[4.8rem]"
-        >
-          <div>
-            <p>{t("contactForm.tower")}</p>
-            <p>{t("contactForm.address")}</p>
-          </div>
-          <p>{t("footer.email")}</p>
-          <p>{t("footer.phone")}</p>
-        </article>
-      </section>
-
-      <section className="px-6 pt-6 pb-12 bg-black lg:px-[6.25rem] lg:pt-[3.125rem] lg:pb-[6.25rem]">
-        <h2
-          style={{ textWrap: "balance" }}
-          className="text-center text-white font-semibold text-[2.5rem] lg:text-[3.5rem]"
-        >
-          {t("contactForm.location")}
-        </h2>
-
-        <div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.380365875!2d106.81728367355424!3d-6.213463560857357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4019a3d835f%3A0x26d6b2e1e8b73d51!2sMayapada%20Tower%202!5e0!3m2!1sen!2sid!4v1747641384991!5m2!1sen!2sid"
-            width="100%"
-            height="460"
-            loading="lazy"
-            className="rounded-2xl mt-[3.125rem] lg:rounded-[50px] lg:mt-[4.7rem]"
-          ></iframe>
-        </div>
       </section>
     </>
   );
--- src/components/contactUs/hero.tsx
+++ src/components/contactUs/hero.tsx
@@ -1,63 +1,33 @@
-import { Typography } from "@material-tailwind/react";
-import ThemeProvider from "../theme-provider";
-import Navbar from "../navbar";
-import headphoneImg from "../../assets/contact-us/headphone.png";
-
 import { useTranslation } from "react-i18next";
+
+import Navbar from "../navbar";
 import "../../i18n";
-import { useEffect } from "react";
+import ThemeProvider from "../theme-provider";
 
-export function Hero() {
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    document.title = t("contactHero.title");
-  }, [i18n.language]);
+export default function ContactUsHero() {
+  const { t } = useTranslation();
 
   return (
     <ThemeProvider>
       <Navbar />
 
-      <header className="min-h-[20rem] w-screen flex items-center bg-white relative bg-hero-contact-us">
-        <div className="container mx-auto grid items-center max-w-[1024px]">
-          <div className="text-center">
-            <Typography
-              variant="h1"
-              className="leading-tight text-white text-[2.5rem] lg:text-[3.5rem]"
-            >
-              {t("contactHero.h1")}
-            </Typography>
-
-            <div className="flex justify-center mt-3 lg:mt-4">
-              <div className="flex justify-center text-white font-normal text-xs items-center border rounded-full border-amber-700 px-2 py-0.5 lg:py-1.5">
-                <a href="/" className="hover:underline">
-                  {t("nav.home")}
-                </a>
-
-                <span className="text-sm mx-2">{">"}</span>
-
-                <span>{t("nav.contactUs")}</span>
-              </div>
+      <header className="flex flex-col justify-center items-center bg-hero bg-cover bg-center min-h-[600px]">
+        <section className="flex flex-col justify-between items-center gap-4 mx-auto py-24 text-white container">
+          <div className="flex justify-center items-center w-full">
+            <div className="bg-[#281343] px-5 py-3 rounded-3xl font-semibold text-white text-sm">
+              Our Contact
             </div>
           </div>
-        </div>
+
+          <h1 className="max-w-[1200px] font-black text-[2.5rem] text-white lg:text-[3.5rem] text-center">
+            {t("contactUsHero.h1")}
+          </h1>
+
+          <p className="max-w-[635px] text-white/90 text-xl text-center">
+            {t("contactUsHero.p")}
+          </p>
+        </section>
       </header>
-
-      <section
-        style={{
-          background:
-            "linear-gradient(142.52deg, #FFFFFF 38.78%, #D4D4D4 70.83%, #FFFFFF 97.47%)",
-        }}
-        className="px-[1.9rem] py-[3.2rem] flex flex-col items-center lg:px-[6.25rem] lg:flex-row-reverse lg:justify-center"
-      >
-        <img src={headphoneImg.src} className="w-[259px] lg:mr-[6.25rem]" />
-
-        <p className="text-center font-semibold text-xl lg:text-3xl lg:text-left">
-          {t("contactHero.p")}
-        </p>
-      </section>
     </ThemeProvider>
   );
 }
-
-export default Hero;
